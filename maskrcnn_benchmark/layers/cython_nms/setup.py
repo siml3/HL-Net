fb80c44d0413607aefa61bbb890a9459357500c8
hello2022 <97286327+siml3@users.noreply.github.com>
2022-10-02 17:39:44 +0800

    Revert "Add files via upload"
    This reverts commit 86e93c40a135bde2958684f5efd6f745b8437db9.

--- maskrcnn_benchmark/layers/cython_nms/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(name="cython_nms", ext_modules=cythonize('cython_nms.pyx'), include_dirs=[numpy.get_include()])
