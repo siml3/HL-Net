dc6af05a4e17784d9afccf45b595fc05666abdbc
hello2022 <97286327+siml3@users.noreply.github.com>
2022-10-02 17:19:07 +0800

    Add files via upload

---
+++ scripts/test_sgcls.sh
@@ -0,0 +1,6 @@
+gpu_id=0,1
+port=10086
+gpu_num=2
+output_dir=""
+
+PYTHONPATH=$PWD CUDA_VISIBLE_DEVICES=${gpu_id} python -m torch.distributed.launch --master_port ${port} --nproc_per_node=${gpu_num} tools/relation_test_net.py --config-file "${output_dir}/config.yml" MODEL.ROI_RELATION_HEAD.USE_GT_BOX True MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL False OUTPUT_DIR "${output_dir}" TEST.IMS_PER_BATCH ${gpu_num} DTYPE "float16" GLOVE_DIR glove
